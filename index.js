d8a4664ea9c63fe8079fefb9de247cd7643fbecb
Nicholas  Luong <nckluong@gmail.com>
2018-06-02 03:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function updateObjectWithKeyAndValue(object, key, value){
   let newObj = object
-  newObj[key] = value
+  newObj.key = value
   return newObj
 }
 
